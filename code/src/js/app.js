b1054938ce74c337bd4e419dc0a5f1e0b4622cb1
Alex Long <alexlong2001@outlook.com>
2019-12-17 20:44:45 +0000

    Computer randomly generates Rock, Paper or Scissors

--- code/src/js/app.js
+++ code/src/js/app.js
@@ -3,20 +3,43 @@ let chosenOption;
 const options = document.querySelectorAll('.option');
 const optionContainer = document.querySelector('.option-container');
 const scoreNumber = document.getElementById('score');
+const competition = document.createElement('div');
 
 options.forEach(option => {
     option.addEventListener('click', (e) => {
         e.preventDefault();
         chosenOption = e.target.id;
         showOption();
-        console.log(options);
     })
 })
 
+const chooseOption = () => {
+
+}
+
+// Computer generated options
+const generatePaper = () => {
+    competition.classList.add('paper');
+    const img = document.createElement('img');
+    img.src = 'img/icon-paper.svg';
+    competition.appendChild(img);
+}
+
+const generateRock = () => {
+    competition.classList.add('rock');
+    const img = document.createElement('img');
+    img.src = 'img/icon-rock.svg';
+    competition.appendChild(img);
+}
+
+const generateScissors = () => {
+    competition.classList.add('scissors');
+    const img = document.createElement('img');
+    img.src = 'img/icon-scissors.svg';
+    competition.appendChild(img);
+}
+
 const showOption = () => {
-    /* options.forEach(option => {
-        option.style.display = 'none';
-    }) */
     if(chosenOption === 'Paper') {
         options[1].style.display = 'none';
         options[2].style.display = 'none';
@@ -30,22 +53,28 @@ const showOption = () => {
         options[2].style.display = 'none';
     } 
 
+    // Shows which option you have chosen
     const chosen = document.createElement('div');
     optionContainer.appendChild(chosen);
     chosen.classList.add('chosen');
-    /* const p = document.createElement('p')
-    chosen.appendChild(p);
-    p.innerHTML = `${chosenOption}` */
-    const competition = document.createElement('div');
+
+    // Shows which option the computer has chosen
     optionContainer.appendChild(competition);
     competition.classList.add('option', 'competition');
+
+    const randomNum = Math.floor(Math.random() * 3);
+    optionsArray[randomNum]();
     
 }
 
+const optionsArray = [
+    generatePaper,
+    generateScissors,
+    generateRock
+]
 
 
-
-
+chooseOption();
 // Displays the current score
 scoreNumber.innerHTML = score;
 
