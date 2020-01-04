ad894a6ef33ed135dd0f26abc35eea733d75f931
Alex Long <alexlong2001@outlook.com>
2020-01-04 19:46:48 +0000

    Generates computers choice when an option has been selected

--- code/src/js/app.js
+++ code/src/js/app.js
@@ -1,20 +1,31 @@
-const optionsContainer = document.querySelector('.option-container');
+// Returns the chosen option
+let chosenOption;
+
+const target = (e) => {
+    e.preventDefault();
+    chosenOption = e.target.id;
+    programRun();
+}
 
 // Generates the starting options
-const generateOptions = (type, img) => {
+const optionsContainer = document.querySelector('.option-container');
+
+const generateOptions = (type) => {
     const option = document.createElement('div');
     optionsContainer.appendChild(option);
     option.classList.add('option', type);
+    option.setAttribute('id', type)
     const optionImg = document.createElement('img');
-    optionImg.src = `img/icon-`+ img + `.svg`
+    optionImg.setAttribute('id', type); // NEEDS FIX
+    optionImg.src = `img/icon-`+ type + `.svg`
     option.appendChild(optionImg);
-    option.addEventListener('click', target); 
+    option.addEventListener('click', target);
 }
 
 const freshBoard = () => {
-    generateOptions('paper', 'paper');
-    generateOptions('scissors', 'scissors');
-    generateOptions('rock', 'rock');
+    generateOptions('paper');
+    generateOptions('scissors');
+    generateOptions('rock');
 }
 
 freshBoard();
@@ -26,18 +37,34 @@ const removeOptions = () => {
     }  
 }
 
-let chosenOption;
-const options = document.querySelector('.option');
+// Generate random option
+const optionsArray = ['paper', 'scissors', 'rock'];
+const randomNum = Math.floor(Math.random() * 3);
 
-// Returns the chosen option
-const target = (e) => {
-    e.preventDefault();
-    chosenOption = e.target.id;
-    programRun();
+// Generates computers option and selects it
+const selectComputersChoice = () => {
+    generateOptions(optionsArray[randomNum]);
+    const computersChoice = optionsContainer.getElementsByTagName('div')[1];
+    console.log(computersChoice);
 }
 
 // Run the program
 const programRun = () => {
-
+    switch(chosenOption) {
+        case 'paper':
+            removeOptions();
+            generateOptions('paper');
+            setTimeout(selectComputersChoice, 500);
+            
+            break;
+        case 'scissors':
+            removeOptions();
+            generateOptions('scissors');
+            break;
+        case 'rock':
+            removeOptions();
+            generateOptions('rock');
+            break;
+    }
 }
 
