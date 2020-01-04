f93a24e42afc1e3d5bbf295f85d36ae2751117cd
Alex Long <alexlong2001@outlook.com>
2020-01-04 17:00:28 +0000

    Making the game save your score, increasing effeciency by rewriting the code

--- code/src/js/app.js
+++ code/src/js/app.js
@@ -1,131 +1,43 @@
-let score = 0;
-let chosenOption;
-const options = document.querySelectorAll('.option');
-const optionContainer = document.querySelector('.option-container');
-const scoreNumber = document.getElementById('score');
-const competition = document.createElement('div');
-const results = document.querySelector('.results h2');
+const optionsContainer = document.querySelector('.option-container');
 
+// Generates the starting options
+const generateOptions = (type, img) => {
+    const option = document.createElement('div');
+    optionsContainer.appendChild(option);
+    option.classList.add('option', type);
+    const optionImg = document.createElement('img');
+    optionImg.src = `img/icon-`+ img + `.svg`
+    option.appendChild(optionImg);
+    option.addEventListener('click', target); 
+}
+
+const freshBoard = () => {
+    generateOptions('paper', 'paper');
+    generateOptions('scissors', 'scissors');
+    generateOptions('rock', 'rock');
+}
+
+freshBoard();
+
+// Remove all the options
+const removeOptions = () => {
+    while (optionsContainer.hasChildNodes()) {
+        optionsContainer.removeChild(optionsContainer.firstChild);
+    }  
+}
+
+let chosenOption;
+const options = document.querySelector('.option');
+
+// Returns the chosen option
 const target = (e) => {
     e.preventDefault();
     chosenOption = e.target.id;
-    showOption();
-};
+    programRun();
+}
 
-options.forEach(option => {
-    option.addEventListener('click', target);
-});
+// Run the program
+const programRun = () => {
 
-// Computer generated options
-const generatePaper = () => {
-    competition.classList.add('paper');
-    const img = document.createElement('img');
-    img.src = 'img/icon-paper.svg';
-    competition.appendChild(img);
-};
-
-const generateRock = () => {
-    competition.classList.add('rock');
-    const img = document.createElement('img');
-    img.src = 'img/icon-rock.svg';
-    competition.appendChild(img);
-};
-
-const generateScissors = () => {
-    competition.classList.add('scissors');
-    const img = document.createElement('img');
-    img.src = 'img/icon-scissors.svg';
-    competition.appendChild(img);
-};
-
-const generateRandomNum = () => {
-    const randomNum = Math.floor(Math.random() * 3);
-    optionsArray[randomNum]();
-};
-
-const showOption = () => {
-    if(chosenOption === 'Paper') {
-        options[1].style.display = 'none';
-        options[2].style.display = 'none';
-    } 
-    if(chosenOption === 'Scissors') {
-        options[0].style.display = 'none';
-        options[2].style.display = 'none';
-    } 
-    if(chosenOption === 'Rock') {
-        options[0].style.display = 'none';
-        options[1].style.display = 'none';
-    } 
-    options.forEach(option => {
-        option.removeEventListener('click', target);
-    });
-    
-
-    // Displays Player 1 vs Player 2
-    document.querySelector('.chosenp-container').style.display = 'flex';
-
-    // Shows which option you have chosen
-    const chosen = document.createElement('div');
-    optionContainer.appendChild(chosen);
-    chosen.classList.add('chosen');
-
-    // Shows which option the computer has chosen
-    optionContainer.appendChild(competition);
-    competition.classList.add('option', 'competition');
-    
-    setTimeout(Computer,1500);
-};
-
-const Computer = () => {
-    generateRandomNum();
-    scoreSystem();
-};
-
-const scoreSystem = () => {
-    document.querySelector('.results').style.display = 'block';
-    if(chosenOption === 'Paper') {
-        if(competition.classList.contains('scissors')){
-            results.innerHTML = 'You Lose'
-        } else if (competition.classList.contains('paper')) {
-            results.innerHTML = 'It\'s a Draw'
-        } else if (competition.classList.contains('rock')) {
-            results.innerHTML = 'You Win';
-            score += 1;
-        }
-    }
-    if(chosenOption === 'Scissors') {
-        if(competition.classList.contains('rock')){
-            results.innerHTML = 'You Lose';
-        } else if (competition.classList.contains('scissors')) {
-            results.innerHTML = 'It\'s a Draw';
-        } else if (competition.classList.contains('paper')) {
-            results.innerHTML = 'You Win';
-            score += 1;
-        }
-    }
-    if(chosenOption === 'Rock') {
-        if(competition.classList.contains('paper')){
-            results.innerHTML = 'You Lose';
-        } else if (competition.classList.contains('rock')) {
-            results.innerHTML = 'It\'s a Draw';
-        } else if (competition.classList.contains('scissors')) {
-            results.innerHTML = 'You Win';
-            score += 1;
-        }
-    }
-    scoreNumber.innerHTML = score;
-};
-
-// Array of functions to generate an option
-const optionsArray = [
-    generatePaper,
-    generateScissors,
-    generateRock
-];
-
-
-
-
-
-
+}
 
